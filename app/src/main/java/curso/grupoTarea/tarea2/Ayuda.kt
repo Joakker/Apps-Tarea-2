b736ebbbab9ba1fd94ebdbd916563576a46ad9dc
Joakker <51842726+Joakker@users.noreply.github.com>
2020-07-03 16:00:12 -0400

    Actualizadas las actividades, unidas ambas lineas de trabajo

--- app/src/main/java/curso/grupoTarea/tarea2/Ayuda.kt
+++ app/src/main/java/curso/grupoTarea/tarea2/Ayuda.kt
@@ -1,7 +1,7 @@
 package curso.grupoTarea.tarea2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
@@ -17,7 +17,7 @@ class Ayuda : AppCompatActivity() {
         adapter.addFragment(Fragmento3() , " A jugar! ")
         viewPager.adapter = adapter
         viewPager.background = getDrawable(R.color.defaultBackground)
-        tabs.setupWithViewPager(viewPager)
+        //tabs.setupWithViewPager(viewPager)
 
     }
 
